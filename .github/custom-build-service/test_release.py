400046fab0b5bd7bb2949a047a934ff765a268f2
Kazuki Nakashima <kazuki.nakashima@nex.nikkei.com>
2026-09-07 19:36:22 +0900

    Apply repository license headers to custom distribution tools
    Use the repository license template for executable sources. Exclude the JSON dependency lock and POSIX shell template from the extension-based checker while retaining a license header in the template.

--- .github/custom-build-service/test_release.py
+++ .github/custom-build-service/test_release.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python3
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the Swift open source project
+##
+## Copyright (c) 2026 Apple Inc. and the Swift project authors
+## Licensed under Apache License v2.0 with Runtime Library Exception
+##
+## See http://swift.org/LICENSE.txt for license information
+## See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+##
+##===----------------------------------------------------------------------===##
+
 """Distribution contract tests; no build or per-user installation is performed."""
 
 import argparse
--- Utilities/Tests/test_custom_xcode_build_service_installer.py
+++ Utilities/Tests/test_custom_xcode_build_service_installer.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
-# Copyright (c) 2026 the Swift project authors.
-# Licensed under Apache License v2.0 with Runtime Library Exception.
-# See https://swift.org/LICENSE.txt for license information.
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the Swift open source project
+##
+## Copyright (c) 2026 Apple Inc. and the Swift project authors
+## Licensed under Apache License v2.0 with Runtime Library Exception
+##
+## See http://swift.org/LICENSE.txt for license information
+## See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+##
+##===----------------------------------------------------------------------===##
 
 """Exercise the download bootstrap without installing a service or contacting GitHub."""
 
